07192114c06ebae8e941bbf9fd0087ba15f9ac4a
imboredidkwhattoputasname <shawn.harts.stu@elyon.sch.id>
2026-04-10 13:20:31 +0700

    Add files via upload

--- stuff.sql
+++ stuff.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 10, 2026 at 08:08 AM
+-- Generation Time: Apr 10, 2026 at 08:19 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `stuff` (
   `first` text NOT NULL,
-  `last name` text NOT NULL,
+  `last` text NOT NULL,
   `email` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 COMMIT;
